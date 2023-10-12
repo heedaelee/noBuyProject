4961ed2086b62382bb08084cc4096220f0fd245c
heedaelee <adguy72@gmail.com>
2023-10-12 16:04:00 +0900

    feat: 🐲 InputField.tsx 내용 추가

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,15 +1,18 @@
+import {Colors, _WIDTH} from 'config/style-config';
 import React from 'react';
 import {StyleSheet, Text, TextInput, TextInputProps, View} from 'react-native';
 
 interface InputFieldProps extends TextInputProps {
+  focused?: {};
   touched?: boolean;
   error?: string;
 }
 
-const InputField = ({touched, error, ...props}: InputFieldProps) => {
+const InputField = ({focused, touched, error, ...props}: InputFieldProps) => {
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} {...props} />
+      {/* TODO: hook 만든 후 styles.inputFocused로 통합하기 */}
+      <TextInput style={[styles.input]} {...props} />
       {/* 에러메시지 */}
       {touched && error && <Text style={styles.error}>{error}</Text>}
     </View>
@@ -17,8 +20,20 @@ const InputField = ({touched, error, ...props}: InputFieldProps) => {
 };
 
 const styles = StyleSheet.create({
-  container: {},
-  input: {},
+  container: {width: '100%'},
+  input: {
+    padding: 5,
+    paddingBottom: 0,
+    marginBottom: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    fontSize: _WIDTH / 30,
+    width: '100%',
+    color: Colors.black,
+  },
+  inputFocused: {
+    borderColor: Colors.blue10,
+    borderBottomWidth: 1,
+  },
   error: {},
 });
 
