64a2d88938ad2a69b0a95d2ca51149f5a1970542
heedaelee <adguy72@gmail.com>
2023-10-19 16:49:45 +0900

    feat: 🐲 입력폼 마무리
    validation에 따른 버튼 활성/비활성화 추가 -> 과정에서 styles. -> Styles(). 로 호출  방법 변경

--- src/screens/BrandFormScreen.tsx
+++ src/screens/BrandFormScreen.tsx
@@ -22,7 +22,7 @@ type RouteProps = HomeTabScreenProps<'BrandFormScreen'>;
 
 export interface BrandFormScreenProps extends RouteProps {}
 
-const BrandFormScreen = ({navigation, route}: BrandFormScreenProps) => {
+const BrandFormScreen = ({navigation}: BrandFormScreenProps) => {
   // useRouteLog();
   /* 헤더 삭제함.  */
   useLayoutEffect(() => {
@@ -39,6 +39,9 @@ const BrandFormScreen = ({navigation, route}: BrandFormScreenProps) => {
 
   const {brandList, page, selectedI} = brandValue;
 
+  /**
+   * brandList 데이터, useForm 훅에 할당하는 작업
+   */
   let initialValue = {name: '', content: ''};
   if (page === 'editor' && selectedI !== '-1') {
     brandList.forEach(v => {
@@ -47,7 +50,20 @@ const BrandFormScreen = ({navigation, route}: BrandFormScreenProps) => {
       }
     });
   }
+  /**
+   * 초기 값 넣고, values랑 input 컨트롤 프로퍼티 리턴해 줌
+   * @param initialValue
+   * @returns {getTextInputProps, values, error, focused}
+   */
   const brandForm = useForm(initialValue);
+
+  /**
+   * submit 버튼 활성화 변수
+   */
+  const canGoNext =
+    !!brandForm.values.name &&
+    !brandForm.error.name &&
+    !brandForm.error.content;
 
   /**
    * 취소 버튼 클릭시
@@ -59,8 +75,6 @@ const BrandFormScreen = ({navigation, route}: BrandFormScreenProps) => {
    * 등록 버튼 클릭시
    */
   const onSubmit = useCallback(async () => {
-    //TODO: 구현 해야함
-    console.log('onSubmit 호출');
     if (loading) {
       return;
     }
@@ -94,8 +108,7 @@ const BrandFormScreen = ({navigation, route}: BrandFormScreenProps) => {
         };
       }
       console.log(JSON.stringify(newBrandValue));
-      console.log('setBrandValue호출');
-      console.log('newBrandValue:');
+      // console.log('setBrandValue호출');
       setBrandValue(newBrandValue);
     } catch (error) {
       Alert.alert(`에러 : ${error}`);
@@ -115,19 +128,19 @@ const BrandFormScreen = ({navigation, route}: BrandFormScreenProps) => {
     navigation,
   ]);
 
-  console.log(`brandForm.values : ${JSON.stringify(brandForm.values)}`);
+  //체크
+  /* console.log(`brandForm.values : ${JSON.stringify(brandForm.values)}`);
   console.log(`brandForm.error : ${JSON.stringify(brandForm.error)}`);
-  console.log(`brandForm.focused : ${JSON.stringify(brandForm.focused)}`);
-
+  console.log(`brandForm.focused : ${JSON.stringify(brandForm.focused)}`); */
   return (
-    <DismissKeyboardView style={styles.DismissKeyboardView}>
-      <View style={styles.block}>
+    <DismissKeyboardView style={styles().DismissKeyboardView}>
+      <View style={styles().block}>
         <View style={globalStyles.titleView}>
-          <Text style={styles.titleText}>
+          <Text style={styles().titleText}>
             {page === 'register' ? '브랜드 추가' : '브랜드 수정'}
           </Text>
         </View>
-        <View style={styles.inputWrapper}>
+        <View style={styles().inputWrapper}>
           <InputField
             placeholder="불매할 브랜드를 입력해주세요*"
             placeholderTextColor="#666"
@@ -159,25 +172,25 @@ const BrandFormScreen = ({navigation, route}: BrandFormScreenProps) => {
             error={brandForm.error.content}
             focused={brandForm.focused.content}
           />
-          <View style={styles.buttonWrapper}>
+          <View style={styles().buttonWrapper}>
             <Pressable
               style={({pressed}) => [
                 {
                   backgroundColor: pressed ? Colors.gray60 : 'white',
                   marginRight: 10,
                 },
-                styles.button,
+                styles().button,
               ]}
               onPress={goBack}>
-              <Text style={styles.buttonText}>취소</Text>
+              <Text style={styles().buttonText}>취소</Text>
             </Pressable>
             <Pressable
               style={StyleSheet.compose(
-                styles.button,
-                styles.registrationButton,
+                styles().button,
+                styles(canGoNext).registrationButton,
               )}
-              onPress={onSubmit}>
-              <Text style={{...styles.buttonText, color: Colors.white}}>
+              onPress={canGoNext ? onSubmit : null}>
+              <Text style={{...styles().buttonText, color: Colors.white}}>
                 {page === 'register' ? '등록' : '수정'}
               </Text>
             </Pressable>
@@ -188,56 +201,57 @@ const BrandFormScreen = ({navigation, route}: BrandFormScreenProps) => {
   );
 };
 
-const styles = StyleSheet.create({
-  DismissKeyboardView: {flex: 1},
-  block: globalStyles.mainBlock,
-  titleText: globalStyles.titleText,
-  inputWrapper: {
-    ...globalStyles.titleView,
-    marginTop: 10,
-  },
-  input: {
-    padding: 5,
-    paddingBottom: 0,
-    marginBottom: 10,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    fontSize: _WIDTH / 30,
-    width: '100%',
-    color: Colors.black,
-  },
-  buttonWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    marginTop: 15,
-    marginBottom: 0,
-    width: '100%',
-  },
-  button: {
-    paddingVertical: 9,
-    paddingHorizontal: 16,
-    alignItems: 'center',
-  },
-  registrationButton: {
-    backgroundColor: '#3f51b5',
-    borderRadius: 4,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 3,
+const styles = (canGoNext?: boolean) =>
+  StyleSheet.create({
+    DismissKeyboardView: {flex: 1},
+    block: globalStyles.mainBlock,
+    titleText: globalStyles.titleText,
+    inputWrapper: {
+      ...globalStyles.titleView,
+      marginTop: 10,
     },
-    shadowOpacity: 0.27,
-    shadowRadius: 10,
-    elevation: 6,
-  },
-  inputFocused: {
-    borderColor: Colors.blue10,
-    borderBottomWidth: 1,
-  },
-  buttonText: {
-    fontSize: 15,
-    color: 'black',
-    fontFamily: 'Cafe24Dangdanghae-v2.0',
-  },
-});
+    input: {
+      padding: 5,
+      paddingBottom: 0,
+      marginBottom: 10,
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      fontSize: _WIDTH / 30,
+      width: '100%',
+      color: Colors.black,
+    },
+    buttonWrapper: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      marginTop: 15,
+      marginBottom: 0,
+      width: '100%',
+    },
+    button: {
+      paddingVertical: 9,
+      paddingHorizontal: 16,
+      alignItems: 'center',
+    },
+    registrationButton: {
+      backgroundColor: canGoNext ? Colors.blue10 : Colors.gray70,
+      borderRadius: 4,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowOpacity: 0.27,
+      shadowRadius: 10,
+      elevation: 6,
+    },
+    inputFocused: {
+      borderColor: Colors.blue10,
+      borderBottomWidth: 1,
+    },
+    buttonText: {
+      fontSize: 15,
+      color: 'black',
+      fontFamily: 'Cafe24Dangdanghae-v2.0',
+    },
+  });
 
 export default BrandFormScreen;
