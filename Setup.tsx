6b319d2f34812efbaabfd512a6e60a4a3a98471f
heedaelee <adguy72@gmail.com>
2023-11-01 17:34:47 +0900

    refactor: ♻️ import Modals 추가, Setup.tsx

--- Setup.tsx
+++ Setup.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import App from './App';
 import {RecoilRoot} from 'recoil';
+import Modals from 'components/Modals/Modals';
 
 const Setup = () => {
   return (
     <RecoilRoot>
+      <Modals />
       <App />
     </RecoilRoot>
   );
