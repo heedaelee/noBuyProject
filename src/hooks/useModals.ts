8a4b8ecf40e9251232e9816962a5d22f9d879be1
heedaelee <adguy72@gmail.com>
2023-11-01 17:36:17 +0900

    refactor: ♻️ modal 바뀐 형태에 따른 자료형 수정 추가

--- src/hooks/useModals.ts
+++ src/hooks/useModals.ts
@@ -1,4 +1,4 @@
-import {useCallback} from 'react';
+import {ComponentProps, FunctionComponent, useCallback} from 'react';
 import {useRecoilState} from 'recoil';
 
 import {modalOpenAtom} from 'store/modal-open';
@@ -7,17 +7,31 @@ const useModals = () => {
   // 모달들의 목록을 보관하는 상태값
   const [modals, setModals] = useRecoilState(modalOpenAtom);
 
-  /* 인자(오픈할 모달 정보 객체)로 모달을 받으면 이를 모달 목록에 (배열의 요소로) 추가하기 */
+  /**
+   * 인자로 모달을 받으면 이를 모달 목록에 (배열의 요소로) 추가하기
+   * 인자는 (오픈할 모달 정보 객체, 컴포넌트 형태로 들어감)
+   */
   const openModal = useCallback(
-    modal => {
-      setModals(modals => [...modals, modal]);
+    <T extends FunctionComponent<any>>(
+      Component: T,
+      props: Omit<ComponentProps<T>, 'open'>,
+    ) => {
+      setModals(modalsPrev => [
+        ...modalsPrev,
+        {Component, props: {...props, open: true}},
+      ]);
     },
     [setModals],
   );
 
-  const closeModal = useCallback(() => {
-    setModals(false);
-  }, [setModals]);
+  const closeModal = useCallback(
+    <T extends FunctionComponent<any>>(Component: T) => {
+      setModals(modals =>
+        modals.filter(modal => modal.Component !== Component),
+      );
+    },
+    [setModals],
+  );
 
   return {
     modals,
--- src/store/modal-open.ts
+++ src/store/modal-open.ts
@@ -1,7 +1,14 @@
+import {ComponentProps, FunctionComponent} from 'react';
+
 import {MODAL_OPEN_KEY} from 'constants/atom-constant';
 import {atom} from 'recoil';
 
-export const modalOpenAtom = atom({
+interface ModalAtomType {
+  Component: FunctionComponent<any>;
+  props: ComponentProps<FunctionComponent<any>>;
+}
+
+export const modalOpenAtom = atom<Array<ModalAtomType>>({
   key: MODAL_OPEN_KEY,
   default: [],
 });
