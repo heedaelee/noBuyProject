bbea4d126fc590e5965cfcb7bdcac6333b71eb08
heedaelee <adguy72@gmail.com>
2023-11-02 20:23:16 +0900

    feat: 🐲 Lazy 적용

--- src/components/Modals/Modals.tsx
+++ src/components/Modals/Modals.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 
 import useModals from 'hooks/useModals';
-import ConfirmModal from './ConfirmModal';
+import {Text} from 'react-native';
+// import ConfirmModal from './ConfirmModal';
+
+/* 코드스플리팅, 동적import */
+const ConfirmModal = lazy(() => import('./ConfirmModal'));
 
 /* 사용할 모달 컴포넌트들을 담은 Object */
 export const modals = {
   confirm: ConfirmModal,
 };
 
+const Loading = <Text>Loading ....</Text>;
+
 const Modals = () => {
   const {modals} = useModals();
   return (
     <>
       {modals.map(({Component, props}, idx) => {
-        return <Component key={idx} {...props} />;
+        return (
+          <Suspense key={idx} fallback={Loading}>
+            <Component key={idx} {...props} />
+          </Suspense>
+        );
       })}
     </>
   );
