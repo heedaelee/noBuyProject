d794254e02c72f39f427d2c5a80977e9db43d3f5
heedaelee <adguy72@gmail.com>
2023-11-08 17:19:31 +0900

    feat: 🐲
    yarn add react-native-splash-screen 모듈 추가
    App.tsx 코드 추가 & pod & yarn.lock & package.json 변경

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import {headerConfig} from 'config/native-config';
 import BottomTabNavigator from 'navigators/BottomTabNavigator';
@@ -11,6 +12,12 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export type InitRouteNameType = 'BottomTabNavigator';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000); //스플래시 활성화 시간
+  });
+
   const initRouteName = 'BottomTabNavigator';
 
   return (
